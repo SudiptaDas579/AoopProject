4588779b5eeebc0ab78f3c49687308b31bfb9d4a
JebaAnnika <jebatahsinannika05@gmail.com>
2025-10-08 22:28:47 +0600

    jeba sync 2

--- src/main/java/org/example/aoopproject/BusFileHandler.java
+++ src/main/java/org/example/aoopproject/BusFileHandler.java
@@ -7,6 +7,17 @@ public class BusFileHandler extends Thread {
 
     public HashSet<BusList>busLists = new HashSet <BusList>();
 
+    static class AppendableObjectOutputStream extends ObjectOutputStream {
+        public AppendableObjectOutputStream(OutputStream out) throws IOException {
+            super(out);
+        }
+        @Override
+        protected void writeStreamHeader() throws IOException {
+            reset();
+        }
+    }
+
+
     @Override
     public void run() {
 
@@ -33,9 +44,33 @@ public class BusFileHandler extends Thread {
         catch(Exception e){
             System.out.println("error in BusFileHandler");
         }
-
-
         return busLists;
     }
+
+    public synchronized void updateInFile(File file,HashSet <BusList>hashSet){
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+            ObjectOutputStream outputStream = file.length() == 0
+                    ? new ObjectOutputStream(fileOutputStream)
+                    : new BusFileHandler.AppendableObjectOutputStream(fileOutputStream);
+
+            for (BusList A : hashSet) {
+                outputStream.writeObject(A);
+                System.out.println("Company lists written in file");
+                outputStream.flush();
+            }
+
+            fileOutputStream.close();
+            outputStream.close();
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error file not found");
+        } catch (IOException e) {
+            System.out.println("Error read/writing file");
+
+        }
+    }
+
 
 }
