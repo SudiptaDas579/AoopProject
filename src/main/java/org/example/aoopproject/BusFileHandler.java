26fe9cc6a8ae3d9c1071eb12aad09003390a988f
JebaAnnika <jebatahsinannika05@gmail.com>
2025-10-08 22:00:58 +0600

    jeba sync 2

--- src/main/java/org/example/aoopproject/BusFileHandler.java
+++ src/main/java/org/example/aoopproject/BusFileHandler.java
@@ -3,26 +3,39 @@ package org.example.aoopproject;
 import java.io.*;
 import java.util.HashSet;
 
-public class BusFileHandler {
+public class BusFileHandler extends Thread {
 
-    private static final String FILE_NAME = "files/buses.txt";
+    public HashSet<BusList>busLists = new HashSet <BusList>();
 
-    // Save all bus objects to file
-    public static void saveBusLists(HashSet<BusList> busLists) throws IOException {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
-            oos.writeObject(busLists);
-        }
+    @Override
+    public void run() {
+
+        File file = new File("org/example/aoopproject/files/CompanyList.txt");
+
+
+
+
     }
 
-    // Load bus objects from file
-    @SuppressWarnings("unchecked")
-    public static HashSet<BusList> loadBusLists() throws IOException, ClassNotFoundException {
-        File file = new File(FILE_NAME);
-        if (!file.exists()) {
-            return new HashSet<>(); // return empty if no file yet
+    public HashSet<BusList> getBusLists(File file)  {
+
+
+        try{
+
+            FileInputStream fileInputStream=new FileInputStream(file);
+            ObjectInputStream objectInputStream=new ObjectInputStream(fileInputStream);
+
+            while (true) {
+
+                busLists.add((BusList) objectInputStream.readObject());
+            }
         }
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
-            return (HashSet<BusList>) ois.readObject();
+        catch(Exception e){
+            System.out.println("error in BusFileHandler");
         }
+
+
+        return busLists;
     }
+
 }
