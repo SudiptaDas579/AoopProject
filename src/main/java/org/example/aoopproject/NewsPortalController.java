06de79e0b2be1cd8d9d4eacb8206c49d64d5ff2c
JebaAnnika <jebatahsinannika05@gmail.com>
2025-10-18 13:59:10 +0600

    HomePage

--- src/main/java/org/example/aoopproject/NewsPortalController.java
+++ src/main/java/org/example/aoopproject/NewsPortalController.java
@@ -1,8 +1,68 @@
 package org.example.aoopproject;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class NewsPortalController {
 
+@FXML
+public Stage stage;
+
+public Scene scene;
 
 
+    @FXML
+    public void homePageSwitch(ActionEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("homepage.fxml"));
+        stage =(Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+
+    }
+
+
+    @FXML
+    public void newsPortalSwitch(ActionEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("NewsPortal.fxml"));
+        stage =(Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+
+    }
+
+    @FXML
+    public void mapSwitch(ActionEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MapView.fxml"));
+        stage =(Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+
+    }
+    @FXML
+    public void eventHolidaySwitch(ActionEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("EventsHolidays.fxml"));
+        stage =(Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+
+    }
+    @FXML
+    public void logOut(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+        stage =(Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+
+    }
 
 }
