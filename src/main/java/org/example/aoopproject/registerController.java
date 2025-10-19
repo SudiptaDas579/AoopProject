89adfc857aba16383bcb11773e5e49ff00107d41
Ahin-Grace <aimahin0110@gmail.com>
2025-10-19 22:49:04 +0600

    register

--- src/main/java/org/example/aoopproject/registerController.java
+++ src/main/java/org/example/aoopproject/registerController.java
@@ -13,7 +13,6 @@ import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
